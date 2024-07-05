0b7828ee4eeefa7c06d77f1baab68c7b6f4a4ac2
UsamaAliAhmedKhan <ualik0316@gmail.com>
2024-07-06 00:18:19 +0500

    added Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { UserInputModule } from "./user-input/user-input.module";
+import { InvestmentResultsModule } from "./investment-results/investment-results.module";
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, InvestmentResultsComponent],
-    imports: [BrowserModule, UserInputModule],
+    declarations: [AppComponent, HeaderComponent],
+    imports: [BrowserModule, UserInputModule, InvestmentResultsModule],
     bootstrap: [AppComponent]
 })
 export class AppModule{}
